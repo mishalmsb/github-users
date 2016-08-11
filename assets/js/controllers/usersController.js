1a3208c92c0543439b65ac7d5e03a9f82455d5c6
Mishal Shashikant <mishalmsb@gmail.com>
2016-08-11 23:40:35 +0100

    pagination hide if no user

--- assets/js/controllers/usersController.js
+++ assets/js/controllers/usersController.js
@@ -8,35 +8,30 @@ function UsersController($http, $scope){
 
   var self            = this;
   self.all            = [];
-  self.query          = "mishal";
-  self.img            = "";
+  self.query          = "";
   self.pege           = 1;
   self.totalPages     = 0;
   self.usersCount     = 0;
   self.currentPage    = 0;
   self.pageSize       = 10;
-
+  self.alert          = "";
   self.searchUsers = function() {
+      self.currentPage = 0;
       $http({
           method : "GET",
-          url : "https://api.github.com/search/users?q=" + self.query + "&page="+ self.pege +"&per_page=20" 
+          url : "https://api.github.com/search/users?q=" + self.query + "&page="+ self.pege +"&per_page=100" 
        }).then(function mySucces(response) {
           self.all = response.data.items;
           self.usersCount = response.data.total_count;
-          self.totalPages = Math.ceil(response.data.total_count / 20);
+          self.totalPages = Math.ceil(self.all.length / 10);
+          if (self.usersCount == 0) {
+              self.alert = "Nothing";
+          }
           console.log(response);
        }, function myError(response) {
           console.log(response);
        });
   }
-
-  self.numberOfPages=function(){
-      return Math.ceil(7);                
-  }
-
-  self.searchUsers();
-  //1410106
-  //10232178
 
   return self;
 }
